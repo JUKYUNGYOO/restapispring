d70aa8f2897fc46e0c2d514bb4a8fd6f161e3284
유주경 <jukyung@yujugyeong-ui-MacBook-Pro.local>
2018-12-06 12:02:14 +0900

    project creation

---
+++ src/main/java/me/whiteship/restapispring/RestapispringApplication.java
@@ -0,0 +1,12 @@
+package me.whiteship.restapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapispringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapispringApplication.class, args);
+    }
+}
